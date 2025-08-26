7673c17206e3e49f1cec99d93c6a64552336eac3
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-08-25 23:30:24 -0600

    [ADD]: Screen limits

--- Bullet Hell Shooter/Assets/Scripts/PlayerController.cs
+++ Bullet Hell Shooter/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float horizontalInput;
     public float verticalInput;
+    
+    public Vector2 Hrange = Vector2.zero;
+    public Vector2 Vrange = Vector2.zero;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,5 +25,15 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+    void LateUpdate() 
+    {
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, Vrange.x, Vrange.y),
+            Mathf.Clamp(transform.position.y, Hrange.x, Hrange.y),
+            transform.position.z
+        );
     }
 }
